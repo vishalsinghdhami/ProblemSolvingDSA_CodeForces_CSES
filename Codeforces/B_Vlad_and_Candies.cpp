d7bb51f577b90defae6d24db795765033ca37ed3
vishal <singhdhamivishal@gmail.com>
2022-04-01 00:45:10 +0530

    do

--- Codeforces/B_Vlad_and_Candies.cpp
+++ Codeforces/B_Vlad_and_Candies.cpp
@@ -5,36 +5,41 @@ int main()
 {
     int t;
     cin >> t;
-    long n;
+    long long n;
 
     for (int i = t; i > 0; i--)
     {
         cin >> n;
-        long arr[n];
+        long long arr[n];
         for (int j = 0; j < n; j++)
         {
             cin >> arr[j];
         }
         sort(arr, arr + n);
-
-        for (int l = 0; l < n / 2; l++)
-        {
-            if (n == 1)
+       //  for (int j = 0; j < n; j++)
+        //{
+          //  cout<< arr[j];
+        //}
+if (n == 1)
             {
-                if (arr[0] == 1)
-                    cout << "YES" << endl;
+                if (arr[0] == 1){
+                    cout << "YES" << endl;}
                 else
-                    cout << "NO" << endl;
+                    {cout << "NO" << endl;}
             }
-            if(n>1){ if((arr[l + 1] - arr[l]) > 1)
+    if(n>1) {    
+            
+             if((arr[n-1] - arr[n-2]) > 1)
             {
                 cout << "NO" << endl;
+                
             }
 
             else
             {
                 cout << "YES" << endl;
+                
             }
-        }
-    }
-}}
+        } }
+    
+}
